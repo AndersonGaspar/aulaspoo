604f9df6d1034e75239f8b1f1921894882e8e18a
Anderson Gaspar de Medeiros <andersongasparm@gmail.com>
2018-09-11 20:15:42 -0300

    Add files via upload

---
+++ Agenda.java
@@ -0,0 +1,40 @@
+public class Agenda{
+
+	private ArrayList<Pessoa> contatos;
+
+	public void addPessoa(Pessoa.p){
+
+	}
+
+	public boolean removePessoa(String n, String s){
+
+	}
+
+	public boolean addTelefone(String r, String n, int pindex){
+
+	}
+
+	public boolean addEmail(String r, String e, int pindex){
+
+	}
+
+	public boolean updateTelefone(String r, String n, int pindex){
+
+	}
+
+	public boolean updateEmail(String r, String e, int pindex){
+
+	}
+
+	public boolean removeTelefone(String r, int pindex){
+
+	}
+
+	public boolean removeEmail(String r, int pindex){
+
+	}
+
+	public void toString(String){
+		
+	}
+}
---
+++ Telefone.java
@@ -0,0 +1,20 @@
+public class Telefone{
+
+	private HashMap<String><String> dados;
+
+	public boolean add(String r, String n){
+
+	}
+
+	public boolean remove(String r){
+
+	}
+
+	public boolean update(String r, String n){
+
+	}
+
+	public void toString(String){
+		
+	}
+}
