0d66b2acb3af9b5295a2c94951dc7886ed2395d2
Anderson Gaspar de Medeiros <andersongasparm@gmail.com>
2018-09-11 20:14:56 -0300

    Pessoa.java

---
+++ Pessoa.java
@@ -0,0 +1,44 @@
+public class Pessoa{
+
+	private String nome;
+	private String sobrenome;
+	private LocalDate dataNasc;
+	public Telefone telefone;
+	public Email email;
+
+
+	public Pessoa(String n, String s, Telefone t, Email e){
+
+	}
+	public Pessoa(String n, String s){
+
+	}
+
+	public boolean addTelefone(String r, String n){
+
+	}
+
+	public boolean addEmail(String r, String e){
+
+	}
+
+	public boolean removeTelefone(String r){
+
+	}
+
+	public boolean removeEmail(String r){
+
+	}
+
+	public boolean updateTelefone(Strin r, String n){
+
+	}
+
+	public boolean updateEmail(String r, String e){
+
+	}
+
+	public void toString(String){
+		
+	}
+}
