617a16cd2ff2cb610b428ed981d7c04a68266d91
Anderson Gaspar de Medeiros <andersongasparm@gmail.com>
2018-09-26 09:09:17 -0300

    Update Telefone.java

--- Telefone.java
+++ Telefone.java
@@ -1,31 +1,37 @@
+package poo;
+
 public class Telefone {
 
-	private int codigo;
-	private String numSerie, modelo;
-	private float peso;
-	private Dimensao dim;
+    private int codigo;
+    private String numSerie, modelo;
+    private float peso;
+    private Dimensao dim;
 
 
-	public Telefone(int c, String s, String m, float p, Dimensao d){
-		
-		this.codigo = c;
-		this.numSerie = s;
-		this.modelo = m;
-		this.peso = p;
-		this.dimensao = d;
-	}
+    public Telefone(int c, String s, String m, float p, Dimensao d){
+
+        this.codigo = c;
+        this.numSerie = s;
+        this.modelo = m;
+        this.peso = p;
+        this.dim = d;
+    }
+
+    public void ola(){
+        System.out.println("Olá, sou um Telefone");
+    }
 
 
-	public void imprimirDados(){
-		
-		System.out.println("Código: " + this.codigo);
-		System.out.println("Número de Série: " + this.numSerie);
-		System.out.println("Modelo: " + this.modelo);
-		System.out.println("Peso: " + this.peso);
-		//System.out.println("Dimenão: " + this.dimensao);
-		
-	}
-	
+    public void imprimirDados(){
+
+        System.out.println("Código: " + this.codigo);
+        System.out.println("Número de Série: " + this.numSerie);
+        System.out.println("Modelo: " + this.modelo);
+        System.out.println("Peso: " + this.peso);
+        //System.out.println("Dimenão: " + this.dimensao);
+
+    }
+
 
 }
 
