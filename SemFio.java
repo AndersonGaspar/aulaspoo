2c60844e0ff06dceb0c0f05f0a823f541e198efd
Anderson Gaspar de Medeiros <andersongasparm@gmail.com>
2018-09-26 09:08:44 -0300

    Update SemFio.java

--- SemFio.java
+++ SemFio.java
@@ -1,26 +1,32 @@
+package poo;
+
 public class Semfio extends Telefone {
 
-	private float frequencia, distancia;
-	private int canais;
+    private float frequencia, distancia;
+    private int canais;
 
 
-	public SemFio(int c, String s, String m, float p, Dimensao d, int ca, float f, float dis){
-		
-		super(c, s, m, p, d);
-		this.frequencia = f;
-		this.distancia = dis;
-		this.canais = ca;
-	}
+    public SemFio(int c, String s, String m, float p, Dimensao d, int ca, float f, float dis){
+
+        super(c, s, m, p, d);
+        this.frequencia = f;
+        this.distancia = dis;
+        this.canais = ca;
+    }
+
+    public void ola(){
+        System.out.println("Olá, sou um Telefone sem fio");
+    }
 
 
-	public void imprimirDados(){
-		
-		super.imprimirDados();
-		System.out.println("Frequência: " + this.frequencia);
-		System.out.println("Distância: " + this.distancia);
-		System.out.println("Canais: " + this.canais);	
-	}
+    public void imprimirDados(){
 
-	
+        super.imprimirDados();
+        System.out.println("Frequência: " + this.frequencia);
+        System.out.println("Distância: " + this.distancia);
+        System.out.println("Canais: " + this.canais);
+    }
+
+
 
 }
