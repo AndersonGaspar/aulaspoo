f3befeeca71083f97a1ef4931c05899f21a4ce78
Anderson Gaspar <andersongasparm@gmail.com>
2018-10-03 08:39:04 -0300

    esse

---
+++ contador/src/main/java/poo/Contador.java
@@ -0,0 +1,30 @@
+package poo;
+
+public class Contador{
+
+    private int valorAtual;
+
+    public Contador(){
+        this.valorAtual = 0;
+    }
+
+    public int getValorAtual(){
+
+        return valorAtual;
+    }
+
+    public void setValorAtual(int valorAtual){
+
+        this.valorAtual = valorAtual;
+    }
+
+//    public Contador(int valorAtual) {
+//        this.valorAtual = valorAtual;
+//    }
+
+    public int incrementaValor(){
+
+        return this.valorAtual = this.valorAtual + 1;
+
+    }
+}
